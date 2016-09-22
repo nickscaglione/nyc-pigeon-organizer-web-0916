a88888a98e632e89ff10c2d4656f98a30561f1a5
Nick Scaglione <nicholas.a.scaglione@gmail.com>
2016-09-22 19:08:00 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,22 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |category, trait_hash|
+    trait_hash.each do |trait, names_array|
+      names_array.each do |name|
+        if pigeon_list[name] != nil
+          pigeon_list[name][category] << trait.to_s
+        else
+          pigeon_list[name] = {
+            :color => [],
+            :gender => [],
+            :lives => []
+          }
+          pigeon_list[name][category] << trait.to_s
+        end
+      end
+    end
+  end
+  pigeon_list
 end
